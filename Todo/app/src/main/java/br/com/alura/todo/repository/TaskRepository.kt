983c0dc20ea62da385e6e94fb3b948c0d0bc7cb9
Alex Felipe <alexfelipevieira@gmail.com>
2019-03-07 12:39:19 -0300

    initial project

---
+++ Todo/app/src/main/java/br/com/alura/todo/repository/TaskRepository.kt
@@ -0,0 +1,21 @@
+package br.com.alura.todo.repository
+
+class TaskRepository {
+
+    private fun add() {
+        TODO("Adicionar tarefa internamente")
+    }
+
+    private fun edit(){
+        TODO("Editar terefa internamente")
+    }
+
+    private fun remove(){
+        TODO("Remover tarefa internamente")
+    }
+
+    private fun findAll(){
+        TODO("Buscar todas tarefas internamente")
+    }
+
+}
